d15c7269119a9ebe73d927012cf6b3f7985c0277
Владислав Харченко <vladislav.harchenko2017@yandex.ru>
2024-01-10 01:47:43 +0700

    main activity updated

--- app/src/main/java/com/bignerdranch/android/movies/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/movies/MainActivity.kt
@@ -15,32 +15,6 @@ import com.bignerdranch.android.movies.ui.theme.MoviesTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MoviesTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MoviesTheme {
-        Greeting("Android")
+        setContentView(R.layout.activity_main)
     }
 }

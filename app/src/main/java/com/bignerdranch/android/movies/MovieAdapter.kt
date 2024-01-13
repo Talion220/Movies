f603f335092e4548183f8b52ec5028f3e3e93eab
Владислав Харченко <vladislav.harchenko2017@yandex.ru>
2024-01-14 01:45:17 +0700

    query v1

--- app/src/main/java/com/bignerdranch/android/movies/MovieAdapter.kt
+++ app/src/main/java/com/bignerdranch/android/movies/MovieAdapter.kt
@@ -8,25 +8,21 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bignerdranch.android.movies.databinding.ItemMovieBinding
 
-class MovieAdapter : ListAdapter<Movie, MovieAdapter.MovieViewHolder>(MovieDiffCallback()) {
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
-        val binding = ItemMovieBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return MovieViewHolder(binding)
-    }
-
-    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
-        val movie = getItem(position)
-        holder.bind(movie)
-    }
-
-    class MovieViewHolder(private val binding: ItemMovieBinding) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(movie: Movie) {
-            binding.movieTitle.text = movie.Title
-            binding.movieYear.text = movie.Year.toString()
-            binding.movieGenre.text = movie.Genre
-            // Здесь вы также можете загружать изображение по URL и устанавливать его в ImageView
+class MovieAdapter : ListAdapter<Movie, MovieAdapter.ViewHolder>(MovieDiffCallback()) {
+    class ViewHolder(private val binding: ItemMovieBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: Movie) {
+            binding.movie = item
+            binding.executePendingBindings()
         }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemMovieBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(getItem(position))
     }
 
 class MovieDiffCallback : DiffUtil.ItemCallback<Movie>() {
